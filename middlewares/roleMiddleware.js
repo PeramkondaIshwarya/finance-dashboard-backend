4b53d0b20cada9f068fd6ebd67f1c0b4a84eaef3
PeramkondaIshwarya <peramakondaiswarya@gmail.com>
2026-04-05 16:23:10 +0530

    Applied .gitignore properly

--- middlewares/roleMiddleware.js
+++ middlewares/roleMiddleware.js
@@ -1,15 +1,3 @@
-// const authorize = (...roles) => {
-//   return (req, res, next) => {
-//     if (!roles.includes(req.user.role)) {
-//       return res.status(403).json({ message: "Forbidden" });
-//     }
-//     next();
-//   };
-// };
-// module.exports = { authorize };
-
-
-// middlewares/roleMiddleware.js
 const authorize = (...roles) => {
   return (req, res, next) => {
     if (!req.user) {
